f7227fef8577f6aca23887ef18a96cef28dbec57
sahaj_shakya <shakyasahaj10@gmail.com>
2024-05-30 10:55:34 +0545

    add api keys

--- yt_app/views.py
+++ yt_app/views.py
@@ -20,19 +20,3 @@ def get_channels(request, channel_name):
                 'maxResults': 10
             }
 
-            response = requests.get(base_url, params=params)
-            if response.status_code == 200:
-                data = response.json()
-
-                channels = []
-                for item in data.get('items', []):
-                    channel = {
-                        'title': item['snippet']['title'],
-                        'description': item['snippet']['description'],
-                        'channel_id': item['id']['channelId'],
-                        'thumbnail_url': item['snippet']['thumbnails']['default']['url']
-                    }
-                    channels.append(channel)
-                return render(request, 'channels.html', {'channels': channels})
-            else:
-                return render(request, 'error.html', {'message': 'Failed to fetch channels'})
